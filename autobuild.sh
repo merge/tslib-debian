50f0fc8f056652d58e63198d00f2fa827f9e4dda
Martin Kepplinger <martin.kepplinger@ginzinger.com>
2017-02-03 10:34:54 +0100

    bump version to 1.4
    tslib-1.4 is released and we use it.

--- autobuild.sh
+++ autobuild.sh
@@ -4,5 +4,5 @@ mkdir -p output/tslib-sources
 cp -a debian output/tslib-sources/
 cd output/tslib-sources
 uscan --force-download
-tar -xvf ../tslib-1.3.tar.bz2 --strip-components=1
+tar -xvf ../tslib-1.4.tar.bz2 --strip-components=1
 debuild -us -uc
