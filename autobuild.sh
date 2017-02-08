6edc859cf1fecf446e7421a6edb21b70b95baa2f
Martin Kepplinger <martin.kepplinger@ginzinger.com>
2017-02-08 15:51:43 +0100

    bump version to 1.5
    This is an important bugfix release.

--- autobuild.sh
+++ autobuild.sh
@@ -4,5 +4,5 @@ mkdir -p output/tslib-sources
 cp -a debian output/tslib-sources/
 cd output/tslib-sources
 uscan --force-download
-tar -xvf ../tslib-1.4.tar.bz2 --strip-components=1
-debuild -us -uc
+tar -xvf ../tslib-1.5.tar.bz2 --strip-components=1
+debuild
