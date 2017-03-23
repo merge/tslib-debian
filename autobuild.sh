3d7a002c59e4449e9a9a85c41de788c1d4cfa3cd
Martin Kepplinger <martink@posteo.de>
2017-03-23 21:29:33 +0100

    autobuild: remove hash check. uscan checks the signature now

--- autobuild.sh
+++ autobuild.sh
@@ -8,25 +8,4 @@ cp -a debian output/tslib-sources/
 cd output/tslib-sources
 uscan --force-download
 tar -xvf ../tslib-${VERSION}.tar.xz --strip-components=1
-cd ..
-wget https://github.com/kergoth/tslib/releases/download/${VERSION}/tslib-${VERSION}.tar.xz.sha256
-sha256sum tslib-${VERSION}.tar.xz > tslib-${VERSION}.hash
-
-DIFF=$(diff tslib-1.8.tar.xz.sha256 tslib-1.8.hash)
-
-if [ "$DIFF" != "" ]
-then
-	echo "===================================="
-	echo "|                                  |"
-	echo "|         Error: bad hash          |"
-	echo "|                                  |"
-	echo "===================================="
-else
-	echo "===================================="
-	echo "|                                  |"
-	echo "|             hash ok              |"
-	echo "|                                  |"
-	echo "===================================="
-fi
-cd tslib-sources
 debuild
