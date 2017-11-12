089440c9b583f32b9a4be735b704a629a099a415
Martin Kepplinger <martink@posteo.de>
2017-11-12 01:23:36 +0100

    autobuild.sh: rename tarball to _orig format for debuild

--- autobuild.sh
+++ autobuild.sh
@@ -10,4 +10,5 @@ cp -a debian output/tslib-sources/
 cd output/tslib-sources
 uscan --force-download
 tar -xvf ../tslib-${VERSION}.tar.xz --strip-components=1
+mv ../tslib-${VERSION}.tar.xz ../tslib_${VERSION}.orig.tar.xz
 debuild
